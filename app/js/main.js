5f6fec793c9c023cddf86d5928e7ee688e8e2c05
Agares <ekeisco@gmail.com>
2014-08-16 13:39:31 +0200

    saving state

--- app/js/main.js
+++ app/js/main.js
@@ -2,27 +2,47 @@
 var canvas = document.getElementById('c');
 var ctx = canvas.getContext('2d');
 var pixelSize = 10;
-
+var i;
 var drawGrilla = function(){
-  for (var i = 0; i < 16*16; i++) {
+  for (i = 0; i < 256; i++) {
     ctx.rect((i%16)*pixelSize, Math.floor(i/16)*pixelSize, pixelSize, pixelSize);
   };
   ctx.stroke();
 }
 
+var Sprite = function(data){
+  var byteArray = new Int16Array(16);
+  var data = data;
+  for (i = 0; i < data.length; i++) {
+    //var y = data[i] >> 4;       // takes the y index 
+    //var x = data[i] & 0XF;      // takes the x index 
+    //byteArray[y] |= 1<<15-x;  // create a number of 16bits each bit represents a pixel of the character
+    byteArray[data[i] >> 4] |= 1<<15-(data[i] & 0XF);  // create a number of 16bits each bit represents a pixel of the character
+  }
+  function rotate(){
+    var byteArrayT = new Int16Array(16);
+    for (var i = 0; i < byteArray.length; i++) {
+      byteArrayT[i]
+    };
+  }
+  return {
+    toByte: byteArray,
+    toData: data
+  }
+}
+
 var drawCharacter = function(array, color, pixelSize){
-  ctx.fillStyle=color;
-  for (var i = 0; i < array.length; i++) {
-    var pixel = array[i];
-    var y = pixel >> 4;
-    var x = pixel & 0XF;
-    ctx.fillRect(x*pixelSize, y*pixelSize, pixelSize, pixelSize);
+  ctx.fillStyle = color;
+  for(i = 0; i < array.length; i++) {
+    //var y = array[i] >> 4;
+    //var x = array[i] & 0XF;
+    ctx.fillRect((array[i] & 0XF)*pixelSize, (array[i] >> 4)*pixelSize, pixelSize, pixelSize);
   };
 }
 
 function loadSprite(sprite, callback){
   var xhr = new XMLHttpRequest;
-  xhr.open('GET', '/sprites/' + sprite, true);
+  xhr.open('GET', 'sprites/' + sprite, true);
   xhr.responseType = 'arraybuffer';
   xhr.onload = function(){
     var data = new Uint8Array(xhr.response || xhr.mozResponseArrayBuffer);
@@ -32,10 +52,12 @@ function loadSprite(sprite, callback){
 }
 
 loadSprite('h', function(data){
-  drawCharacter(data, '#000', 10);
+  var hero = new Sprite(data);
+  drawCharacter(hero.toData, '#000', 10);
 });
 
 loadSprite('f', function(data){
-  drawCharacter(data, '#E60', 4);
+  var fire = new Sprite(data)
+  drawCharacter(fire.toData, '#E60', 4);
 });
 
