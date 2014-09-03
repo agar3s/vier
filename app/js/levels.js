b3620ad475b7da1ade044a1bedde66cdbd4d794b
Agares <ekeisco@gmail.com>
2014-09-03 16:53:30 +0200

    improves the collision

--- app/js/levels.js
+++ app/js/levels.js
@@ -3,14 +3,16 @@ var Level = function(){
   m.w = 1500;
   m.h = 0;
   //m.points = [];
-  var factor = 18;
+  var factor = 32;
   //var montains = +20*(2*Math.sin(i/3));
   //var imposible climbing = -i/2+30*(2*Math.tan(i));
+  //var y = -20*(2*Math.sin(i/3));
   m.generateLevel = function(){
     platforms = [];
     for (var i = 0; i <= m.w; i+=factor) {
       var grad = i%(Math.PI*2);
-      var y = -20*(2*Math.sin(i/3));
+      //var y = -20*(2*Math.sin(i/3));
+      var y = -i/4+20*(2*Math.tan(i));
       m.h=y<m.h?y:m.h;
       platforms.push(new Platform(i,y, factor));
       //m.points.push({x:i,y:300+(factor)*(Math.sin(i)-Math.cos(i))});
@@ -21,14 +23,19 @@ var Level = function(){
     var index = Math.floor(-viewport.x/factor);
     if(index<0){index=0;}
     var limit = index + Math.floor(dimensions.w/factor);
+    ctx.fillStyle='#A44';
     for(i = index; i <= limit; i++) {
       platforms[i].draw();
     }
   }
   m.collides = function(){
-   for(i = 0; i < platforms.length; i++) {
+    var index = Math.floor(heroS.xi()/factor)-1;
+    if(index<0)index=0;
+    var limit = Math.floor(heroS.xf()/factor)+1;
+    if(limit>=platforms.length) limit = platforms.length-1;
+    for(i = index; i <= limit; i++) {
       platforms[i].collides();
-    } 
+    }
   }
   m.drawLevel = function(){
   }
--- app/js/platform.js
+++ app/js/platform.js
@@ -3,10 +3,11 @@ var Platform = function(x,y,width){
   m.x = x;
   m.y = y;
   m.width = width;
-  m.collide = 0;
+  m.collide = 1;
   m.collides = function(){
     if((heroS.xi()>m.x&&heroS.xi()<m.x+m.width||
-      heroS.xf()>m.x&&heroS.xf()<m.x+m.width)&&
+      heroS.xf()>m.x&&heroS.xf()<m.x+m.width||
+      heroS.xi()<m.x&&heroS.xf()>m.x+m.width)&&
       (m.y>heroS.yil()&&m.y+5<heroS.yf())&&
       (heroS.vy>=0)){
       m.collide = 1;
@@ -17,9 +18,9 @@ var Platform = function(x,y,width){
     }
   },
   m.draw = function(){
-    //ctx.fillStyle=m.collide?'#F00':'#668';
-    //ctx.fillRect(m.x, m.y, m.width, 500);
-    ctx.fillStyle=m.collide?'#F44':'#663';
+    //ctx.fillStyle=m.collide?'#700':'#668';
+    ctx.fillRect(m.x, m.y, m.width, -xlevel.h+dimensions.w);
+    
     ctx.fillRect(m.x, m.y, m.width, 5);
   }
 }
